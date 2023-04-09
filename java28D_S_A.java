d6610b90172eb7709d64f3f3cbd31ce963ddf4fb
RamlalNikam <chiragnikam02@gmail.com>
2023-04-09 07:50:01 +0530

    Kadnies Algorithem

--- java28D_S_A.java
+++ java28D_S_A.java
@@ -3,17 +3,15 @@ class java28D_S_A{
 	public static void main(String[] args){
 		int mini, maxi;
 		int arr[] = {1, 2, 3, -2, 5};
-		maxi = mini = arr[0];
-		for(int i = 1; i < arr.length; i++){
+		maxi = Integer.MIN_VALUE;
+		mini = 0; 
+		for(int i = 0; i < arr.length; i++){
 			mini = mini + arr[i];
 			if(mini > maxi)
 				maxi = mini;
-			else if(arr[i] < 0){
+			if(mini < 0){
 				mini = 0;
-				if(maxi > 0)
-					maxi = maxi + arr[i];
-			} else
-				maxi = maxi + arr[i];
+			} 
 		}
 		System.out.println(maxi);
 	}
